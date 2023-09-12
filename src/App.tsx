62750c2b2836c48818a80179d14d47093dd08a35
Tamiris Beira <tamirisbeira@gmail.com>
2023-09-12 13:58:24 -0300

    Configurando o aside

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import {Github}  from "lucide-react"
+import { Github, FileVideo, Upload }  from "lucide-react"
 import { Button } from "./components/ui/button"
 import { Textarea } from "./components/ui/textarea"
+import { Separator } from "./components/ui/separator"
+import { Label } from "@radix-ui/react-label"
 export function App() {
 
   return (
@@ -32,7 +34,33 @@ export function App() {
               Lembre-se: você pode utilizar a variável <code className="text-violet-400">{"{transcriptions}"}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
             </p>
           </div>
-          <aside className="w-80"></aside>
+          <aside className="w-80 space-y-6">
+            <form className="space-y-6">
+              <label htmlFor="video" className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/50">
+                Selecione um vídeo <small>formato .mp4</small>
+                <FileVideo className="w-4 h-4" />
+              </label>
+              <input type="file" name="video" id="video" accept="video/mp4" className="sr-only" />
+              <Separator />
+
+              <div className="space-y-1">
+                <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
+                <Textarea 
+                  id="transcription_prompt" 
+                  className="min-h-20 leading-relaxed" 
+                  placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)."
+                  />
+
+                  <Button type="submit" className="w-full">
+                    Carregar vídeo 
+                    <Upload className="w-4 h-4 ml-2"/>
+                  </Button>
+              </div>
+
+            </form>
+
+            
+          </aside>
         </main>
       </div>
     </>
